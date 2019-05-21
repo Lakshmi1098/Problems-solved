4a6aef9ecd90f5749aa4ecd50d93e066190b375a
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-21 12:30:09 +0530

    Create Perfect numbers or not.py

---
+++ Perfect numbers or not.py
@@ -0,0 +1,26 @@
+ 
+def isPerfect( n ): 
+	
+	# To store sum of divisors 
+	sum = 1
+	
+	# Find all divisors and add them 
+	i = 2
+	while i * i <= n: 
+		if n % i == 0: 
+			sum = sum + i + n/i 
+		i += 1
+	
+	# If sum of divisors is equal to 
+	# n, then n is a perfect number 
+	
+	return (True if sum == n and n!=1 else False) 
+
+# Driver program 
+
+n=12
+if isPerfect (n): 
+    print(n , " is a perfect number") 
+else:
+    print("Not perfect number")
+# This code is contributed by "Sharad_Bhardwaj". 

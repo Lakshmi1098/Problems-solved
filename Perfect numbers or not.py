325d9d4e906c72f0b80fab5059337e802df9ca70
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-05-21 16:16:25 +0530

    Update Perfect numbers or not.py

--- Perfect numbers or not.py
+++ Perfect numbers or not.py
@@ -1,26 +1,15 @@
+import math
+t=int(input()) 
+for i in range(t):
+    n=int(input())
+    sum=1
+    j=2
+    while(j<=int(math.sqrt(n))):
+        if(n%j==0):
+            sum=sum+j+int(n/j);
+        j=j+j
+    if(sum==n):
+        print(1) 
+    else:
+        print(0)
  
-def isPerfect( n ): 
-	
-	# To store sum of divisors 
-	sum = 1
-	
-	# Find all divisors and add them 
-	i = 2
-	while i * i <= n: 
-		if n % i == 0: 
-			sum = sum + i + n/i 
-		i += 1
-	
-	# If sum of divisors is equal to 
-	# n, then n is a perfect number 
-	
-	return (True if sum == n and n!=1 else False) 
-
-# Driver program 
-
-n=12
-if isPerfect (n): 
-    print(n , " is a perfect number") 
-else:
-    print("Not perfect number")
-# This code is contributed by "Sharad_Bhardwaj". 

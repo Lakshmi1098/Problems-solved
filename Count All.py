b90efb56414a41e754ce9f6f2e27a5cd73478fb4
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-06-01 11:58:05 +0530

    Update Count All.py

--- Count All.py
+++ Count All.py
@@ -14,4 +14,20 @@ Ar = list(map(int, input().split()))
 
 out_ = solve(Ar)
 print (out_)
+
+
+*************EFFICIENT METHOD*********************
+n=int(input())
+a=list(map(int,input().split()))
+MAX=max(a)
+s=[]
+count=[0]*(MAX+1)
+s=[0]*(n+1)
+for i in range(n):
+    count[a[i]]+=1
+for i in range(1,n+1):
+    for j in range(i,MAX+1,i):
+        s[i]+=count[j]
+b=sum(s)
+print(b)
         

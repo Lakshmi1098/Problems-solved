fb3007ec6bea9f4fc1e50a0bf972859c469c7c3e
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-06-07 17:17:18 +0530

    Create Reverse number.java

---
+++ Reverse number.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner lp=new Scanner(System.in);
+        int num=lp.nextInt();
+        int reversed=0;
+         while(num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println(reversed);
+    }
+}

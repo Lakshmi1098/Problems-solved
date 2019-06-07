e17404896004ccd559e1cfa06be395fb3d259897
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-06-07 16:42:57 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+n=int(input())
+n1=0
+n2=1 
+c=0
+if(n==1):
+    print(n1)
+else:
+    while(c<n):
+        print(n1,end=" ")
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        c+=1
+        

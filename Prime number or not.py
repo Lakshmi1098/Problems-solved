6f6695b5319f8cc9b05f7e82400fb84eee26a80f
Lakshmi.R <46350353+Lakshmi1098@users.noreply.github.com>
2019-07-03 08:41:05 +0530

    Update Prime number or not.py

--- Prime number or not.py
+++ Prime number or not.py
@@ -9,7 +9,7 @@ if(num>1):
     print("Prime number")
     
     
-    OPTIMIZED SOLUTION:
+ ******************USING FUNCTION:**************
     def prime(num):
     if(num<=1):
         return ("Not Prime")
@@ -25,3 +25,25 @@ if(num>1):
     
 num=int(input())
 print(prime(num))
+
+
+
+****************OPTIMIZED SOLUTION:**************
+import math as m
+def prime(n):
+    if n<=1:
+        return False
+    elif n==2 or n==3:
+        return n
+    elif n>2 and n%2==0:
+        return False
+        
+    d=m.floor(m.sqrt(n))
+    for j in range(5,d):
+        if n%i==0:
+            return False
+    return n
+
+
+n=int(input())
+print(prime(n))
